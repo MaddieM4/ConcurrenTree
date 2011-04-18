4e2cfe7cfa228168c3a9f56cc6219c7c3d9ab6ca
Philip Horger <campadrenalin@gmail.com>
2011-04-18 11:46:05 -0700

    Various upgrades and bugfixes to Address class

--- address.py
+++ address.py
@@ -3,15 +3,19 @@ import re
 class Address:
 	''' Address format: index:hash/index:hash '''
 
-	def __init__(self, string):
+	def __init__(self, target):
 		self.layers = []
-		if not self.process(string):
-			raise ValueError(string+" is not a valid address of form index:hash")
+		if type(target)==str:
+			if not self.process(target):
+				raise ValueError(target+" is not a valid address of form index:hash")
+		elif type(target) == Address:
+			self.layers = target.layers
 
 	def process(self, string):
+		if len(string) == 0:
+			# Root tree
+			return True
 		pieces = string.split("/")
-		if len(pieces) == 0:
-			return False
 		for i in pieces:
 			match = re.match("^(\d+):([0-9a-fA-F]+)$", i)
 			if match:
@@ -26,3 +30,6 @@ class Address:
 		for i in self.layers:
 			x = x.get(i[0],i[1])
 		return x
+
+	def __str__(self):
+		return "/".join(["%d:%s" % i for i in self.layers])
