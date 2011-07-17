86a31923377d09f18e1efb3385f76734397abcce
Philip Horger <campadrenalin@gmail.com>
2011-07-16 18:00:13 -0700

    Finally have a Policy class I'm comfortable with

--- BCP/serverpool.py
+++ BCP/serverpool.py
@@ -114,4 +114,51 @@ class PoolServer:
 
 class Policy:
 	''' A Server policy is a bit like a config file. '''
-	pass
+	def __init__(self, inputs = {}, outputs = {}):
+		self.inputs = SubPolicy(inputs)
+		self.outputs = SubPolicy(outputs)
+
+	def input(self, message, *args):
+		self.run(self.inputs, message, *args)
+
+	def output(self, message, *args):
+		self.run(self.outputs, message, *args)
+
+	def run(self, ivo, message, *args):
+		try:
+			func = ivo[message['type']]
+		except KeyError:
+			return None
+		return func(message, *args)
+
+class SubPolicy(dict):
+	''' Contains functions keyed on message type, is thread-safe '''
+	def __init__(self, values={}):
+		super(SubPolicy, self).__init__(self)
+		self.lock = Lock()
+		self.update(values)
+
+	def __getitem__(self, i):
+		with self.lock:
+			return super(SubPolicy, self)[i]
+
+	def __setitem__(self, i, y):
+		# validate value
+		if type(i)!=str:
+			raise TypeError("SubPolicy keys must be strings")
+		if not hasattr(y,"__call__"):
+			raise TypeError("SubPolicy values must be callable")
+		with self.lock:
+			super(SubPolicy, self)[i] = y
+
+	def __delitem__(self, i):
+		with self.lock:
+			del super(SubPolicy, self)[i]
+
+	def set_multi(self, value, *args, keys=[]):
+		for i in args + keys:
+			self[i] = value
+
+	def bloom(self, source, *args, keys=[]):
+		for i in args+keys:
+			self[i] = self[source]
