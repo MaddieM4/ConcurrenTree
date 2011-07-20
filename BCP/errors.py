a3294b18a8ffdf0d75544d840b4e5cda124fecb0
Philip Horger <campadrenalin@gmail.com>
2011-07-20 10:15:14 -0700

    More work on error types

---
+++ BCP/errors.py
@@ -0,0 +1,13 @@
+errors = {
+	400:"Bad message",
+	401:"Unknown message type",
+	402:"Document moved temporarily",
+	403:"Document moved permanently",
+	404:"Document could not be found",
+	451:"Could not parse JSON",
+	452:"Missing required argument",
+	453:"Wrong argument type",
+	454:"JSON root type must be an Object, {}",
+
+	500:"Unknown local error"
+}
