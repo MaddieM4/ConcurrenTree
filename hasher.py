5ad76ed360265f3651353255bafd1ee2669a3625
Philip Horger <campadrenalin@gmail.com>
2011-07-21 16:57:06 -0700

    Added a few convenience functions to hasher.py

--- hasher.py
+++ hasher.py
@@ -4,3 +4,9 @@ HASH_FUNCTION = 'sha256' # was md5
 
 def make(string):
 	return new(HASH_FUNCTION, string).hexdigest()
+
+def make6(string):
+	return maken(string, 6)
+
+def maken(string, n):
+	return make(string)[:n]
