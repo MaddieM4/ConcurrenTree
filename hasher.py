1c1168dde70ce6896e4ea113fdf27348eb228544
Philip Horger <campadrenalin@gmail.com>
2011-07-26 16:26:12 -0700

    Added key and sum functions to hasher.py

--- hasher.py
+++ hasher.py
@@ -10,3 +10,15 @@ def make6(string):
 
 def maken(string, n):
 	return make(string)[:n]
+
+def sum(string):
+	s = 0
+	for i in string:
+		s += ord(i)
+	return s
+
+def key(string):
+	if len(string)>10:
+		return string[:10]+str(sum(string[10:]))
+	else:
+		return string
