eb1d1b35edbbacb8ee452e0ca94a8c7b66389b1c
Philip Horger <campadrenalin@gmail.com>
2011-07-27 19:09:40 -0700

    Created a simple JSON equivalence check algorithm

--- hasher.py
+++ hasher.py
@@ -1,4 +1,5 @@
 from hashlib import new
+import json
 
 HASH_FUNCTION = 'sha256' # was md5
 
@@ -11,11 +12,36 @@ def make6(string):
 def maken(string, n):
 	return make(string)[:n]
 
-def sum(string):
+modulo = 2**16
+
+def sum(string, verbose = False):
 	s = 0
 	for i in string:
-		s += ord(i)
+		if verbose: print s
+		s = (s*s + ord(i)) % modulo
 	return s
+
+def strict(obj):
+	t = type(obj)
+	if t==bool or obj==None or t==str or t==int or t==unicode:
+		return json.dumps(obj)
+	if t==list or t==tuple:
+		return "[%s]" % ",".join([strict(x) for x in obj])
+	if t==dict:
+		strdict = {}
+		for key in obj:
+			strdict[str(key)] = obj[key]
+		keys = strdict.keys()
+		keys.sort()
+		return "{%s}" % ",".join([strict(key)+":"+strict(strdict[key]) for key in keys])
+	else:
+		raise TypeError("Not JSONable: "+str(t))
+
+def strictify(jsonstring):
+	return strict(json.loads(jsonstring))
+
+def checksum(obj):
+	return sum(strict(obj))
 
 def key(string):
 	if len(string)>10:
