4744ec3ad9a1df91a05c034c38da0433fabbd42f
Philip Horger <campadrenalin@gmail.com>
2011-07-30 15:12:07 -0700

    Made a simple install script for python 2.6/linux. Not very flexible and should be improved through internet research.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
-from ctree import CTree
-from marker import Marker
-from marker import Default as MarkerDefaults
-from operation import Operation
-import instruction
+from model import *
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ln -s `pwd` /usr/lib/pymodules/python2.6/ConcurrenTree
+chmod +x `pwd`/orchard/orchard.py
+ln -s `pwd`/orchard/orchard.py /usr/bin/orchard
