309912735c2447686f5bf63577181677abdae08e
Philip Horger <campadrenalin@gmail.com>
2011-09-25 00:33:24 -0700

    Updated address format in address.py

--- model/address.py
+++ model/address.py
@@ -6,37 +6,42 @@ class Address(ModelBase):
 	''' Address format: [[index, key], [index, key]] '''
 
 	def __init__(self, target):
-		self.layers = []
 		if type(target)==list:
-			self.layers = target
+			self.parse(target)
 		elif isinstance(target, Address):
 			self.layers = target.layers
-		elif type(target)==str:
+		elif type(target) in (str, unicode):
 			self.layers = json.loads(target)
 		else:
 			raise TypeError("Expected list or address.Address, got "+str(type(target)))
 
+	def parse(self, l):
+		''' Does not check for syntax errors yet '''
+		self.layers = []
+		pos = None
+		for i in l:
+			if type(i)==int:
+				pos = int
+			else:
+				if pos==None:
+					self.layers.append(i)
+				else:
+					self.layers.append((pos,i))
+					pos = None
+
 	def resolve(self, tree):
 		x = tree
 		for i in self.layers:
+			if type(i) in (str, unicode):
+				i = (len(x), i)
 			x = x.get(i[0],i[1])
 		return x
 
 	def proto(self):
-		return self.layers
-
-def validate_shortcut(shortcut):
-	try:
-		if (type(shortcut) in (str, unicode)):
-			shortcut = shortcut.split("#")
-		assert(type(shortcut)==list or type(shortcut)==tuple)
-		assert(len(shortcut)==2)
-		assert(type(shortcut[0])==int and shortcut[0]>=0)
-		assert(type(shortcut[1]) in (str, unicode))
-	except AssertionError:
-		raise InvalidShortcutError(shortcut)
-	return tuple(shortcut)
-
-class ShortcutError(Exception): pass
-class InvalidShortcutError(ShortcutError): pass
-class ShortcutUnresolvedError(ShortcutError): pass
+		result = []
+		for i in self.layers:
+			if type(i)==tuple:
+				result += list(i)
+			else:
+				result.append(i)
+		return result
