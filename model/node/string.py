dcdcf7d85ea29c93774013ca24d8295e79c908c5
Philip Horger <campadrenalin@gmail.com>
2011-11-14 20:25:34 -0800

    Finally realized that string.py got lost in an ancient merge and I've been running off an old pyc without realizing. Restored from old commit now but finding the most up-to-date backup was an exercise in panic.

---
+++ model/node/string.py
@@ -0,0 +1,23 @@
+from linear import LinearNode
+
+class StringNode(LinearNode):
+
+	def __init__(self, value):
+		try:
+			value = str(value)
+		except:
+			raise TypeError("ListNode value must str, or something that can be turned into one")
+		LinearNode.__init__(self, value, StringNode) # can only contain other StringNodes
+
+	def encapsulate(self, obj):
+		return str(obj)
+
+	def flatitem(self, i):
+		return self[i]
+
+	@property
+	def key(self):
+		return self.keysum("t"+self.value)
+
+	def proto(self):
+		return [] # TODO - advanced type representations
