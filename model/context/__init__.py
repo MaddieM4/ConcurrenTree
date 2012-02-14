7f87e779075509907f04431372f8fc540899e1e6
Philip Horger <campadrenalin@gmail.com>
2012-02-13 22:12:36 -0800

    SingleNode context done, fixed some MapNode bugs

--- model/context/__init__.py
+++ model/context/__init__.py
@@ -1,5 +1,5 @@
 from ConcurrenTree.model import node
-import string, map
+import string, map, single
 
 def make(node_obj):
 	# Create a context object
@@ -8,3 +8,5 @@ def make(node_obj):
 		return string.StringContext(node_obj)
 	elif t == node.MapNode:
 		return map.MapContext(node_obj)
+	elif t == node.SingleNode:
+		return single.SingleContext(node_obj)
--- model/context/map.py
+++ model/context/map.py
@@ -2,15 +2,33 @@ from ConcurrenTree.model import operation, instruction, node
 import context
 
 class MapContext(context.Context):
+
+	# Model level - accurate to data model
+
+	def get(self, key):
+		return self.node.get(key, "/single")
+
+	def set(self, key, value):
+		if key in self:
+			# Use existing SingleNode
+			return [key, "/single"]+self.get(key).context().set(value)
+		else:
+			# Make Singlenode
+			s = instruction.InsertSingle([], key)
+			# Make actual node
+			n = instruction.InsertNode([key, "/single"], 0, node.make(value))
+			return operation.Operation([s,n])
+
+	def has(self, key):
+		return key in self.node._data
+
+	# Virtual level - treat null values as removed, drop ops, etc.
+
 	def __getitem__(self, key):
-		return self.node.get(key, "/single").flatten()
+		return self.get(key).flatten()
 
 	def __setitem__(self, key, value):
 		self.live.set(key, value)
 
-	def set(self, key, value):
-		# Make Singlenode
-		s = instruction.InsertSingle([], key)
-		# Make actual node
-		n = instruction.InsertNode([key, "/single"], 0, node.make(value))
-		return operation.Operation([s,n])
+	def __contains__(self, key):
+		return self.has(key) and (self[key] != None)
