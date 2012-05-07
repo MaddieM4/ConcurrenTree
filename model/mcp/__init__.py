e32281befb5c8c021ad8cb078768b7bdd2e856ed
Philip Horger <campadrenalin@gmail.com>
2012-05-07 13:12:40 -0700

    Got the first two sections of the MCP unit test running, and I don't intend to add the third until the RSA problems have been worked out in their own ticket.

--- model/mcp/__init__.py
+++ model/mcp/__init__.py
@@ -14,16 +14,50 @@ are running in the same process and the same MCP router.
 w<{'content': {}, 'routing': {}}>
 >>> gbrg.host_table
 w<{'content': {}, 'routing': {}}>
+
 >>> localip = '127.0.0.1'
 >>> bob = ['udp4', [localip, 3939], "bob"]
 >>> bridget = ['udp4', [localip, 3940], "bridget"]
+
 >>> gbrg.client(bridget, ["rotate", 7]) #doctest: +ELLIPSIS
 <ConcurrenTree.model.mcp.client.SimpleClient object at 0x...>
 >>> gbob.client(bob, ["rotate", 3]) #doctest: +ELLIPSIS
 <ConcurrenTree.model.mcp.client.SimpleClient object at 0x...>
+
 >>> gbrg.resolve_set(bob, ["rotate", 3])
 >>> gbrg.hello(bob)
+
 >>> gbrg.dm(bob, "Hello, Bob!")
 Direct message from [u'udp4', [u'127.0.0.1', 3940], u'bridget']
 u'Hello, Bob!'
+>>> gbob.dm(bridget, "Hey there, Bridget :)")
+Direct message from [u'udp4', [u'127.0.0.1', 3939], u'bob']
+u'Hey there, Bridget :)'
+
+### Track 1 Ops
+
+>>> helloname = gbob.mkname(bob, "hello")
+>>> hellobob  = gbob.document(helloname)
+>>> hellobrg  = gbrg.document(helloname)
+>>> hwbob = hellobob.content
+>>> hwbrg = hellobrg.content
+
+>>> gbob.add_participant(helloname, bridget)
+>>> hwbob["goofy"] = "gorsh"
+
+>>> hwbrg
+w<{u'goofy': 'gorsh'}>
+>>> gbrg.can_read(bridget, helloname)
+True
+>>> hwbrg["Blabarsylt"] = "Swedish jelly"
+>>> hwbrg["Blabarsylt"] = "Made of blueberries"
+
+>>> hwbob["Blabarsylt"]
+w<'Made of blueberries'>
+>>> hellobob.routes_to(bob)
+[[u'udp4', [u'127.0.0.1', 3940], u'bridget']]
+
+### RSA
+
+# Will do later
 '''
