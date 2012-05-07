60f34d24889436bd0bdb5a8554027ee419c0946a
Philip Horger <campadrenalin@gmail.com>
2012-05-07 09:49:24 -0700

    Fixed bug10: Reduce default output of recursive tester

--- test.py
+++ test.py
@@ -3,17 +3,24 @@
 import doctest
 import ConcurrenTree
 
-def test_recursive(mod):
+def test_recursive(mod, debug=False):
 	(failure_count, test_count) = doctest.testmod(mod)
 	if "__all__" in dir(mod):
 		for child in mod.__all__:
 			fullchildname = mod.__name__+"."+child
-			print "Testing", fullchildname
+			if debug:
+				print "Testing", fullchildname
 			childmod = __import__(fullchildname, fromlist=[""])
-			cf, ct = test_recursive(childmod)
+			cf, ct = test_recursive(childmod, debug)
 			failure_count += cf
 			test_count    += ct
 	return (failure_count, test_count)
 
-failures, tests = test_recursive(ConcurrenTree)
-print "%d failures, %d tests." % (failures, tests)
+def test_ctree(debug = False):
+	failures, tests = test_recursive(ConcurrenTree, debug)
+	print "%d failures, %d tests." % (failures, tests)
+
+if __name__ == "__main__":
+	import sys
+	debug = "-l" in sys.argv
+	test_ctree(debug)
