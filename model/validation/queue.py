bcae88173ec7c871faf49d55492d5c50050977f0
Philip Horger <campadrenalin@gmail.com>
2012-05-08 00:14:07 -0700

    Changed to ValidationQueue, added a little documentation.

--- model/validation/queue.py
+++ model/validation/queue.py
@@ -1,14 +1,13 @@
 from Queue import Queue, Empty
 
-class ValidatorQueue(object):
+class ValidationQueue(object):
 	'''
 	Class capable of taking an iterable (even a generator) and
 	acting as an iterable queue. Iteration will end - not stop -
 	if you get to the end of the current contents of the internal
 	queue, so expect to for-loop through an instance multiple times.
 
-	>>> #from ConcurrenTree.model.validator import queue
-	>>> my_queue = ValidatorQueue(xrange(1,5))
+	>>> my_queue = ValidationQueue(xrange(1,5))
 	>>> for i in my_queue:
 	...     print i
 	...     if i < 5:
@@ -25,7 +24,9 @@ class ValidatorQueue(object):
 	8
 	8
 	'''
+
 	def __init__(self, source=[]):
+		# Accepts any iterable as optional argument for initial data.
 		self.source = source
 		self.queue = Queue()
 		self.generator = self.gen()
@@ -33,7 +34,11 @@ class ValidatorQueue(object):
 	def __iter__(self):
 		return self
 
+	def next(self):
+		return self.generator.next()
+
 	def gen(self):
+		# Returns the generator used by next().
 		for x in self.source:
 			yield x
 		self.source = []
@@ -43,8 +48,6 @@ class ValidatorQueue(object):
 			except Empty:
 				return
 
-	def next(self):
-		return self.generator.next()
-
 	def add(self, obj):
+		# Add an object to the internal queue.
 		self.queue.put(obj)
