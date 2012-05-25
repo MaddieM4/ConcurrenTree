ee740f712651c2d171c22cadea7444d756cec6df
Philip Horger <campadrenalin@gmail.com>
2012-05-25 14:01:56 -0700

    Created setup.py, updated .gitignore. Closes #24

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='ConcurrenTree',
+	version='0.4',
+	long_description=open('README.md').read(),
+	packages = [
+		'ConcurrenTree',
+		'ConcurrenTree.model',
+		'ConcurrenTree.model.bcp',
+		'ConcurrenTree.model.context',
+		'ConcurrenTree.model.mcp',
+		'ConcurrenTree.model.node',
+		'ConcurrenTree.model.validation',
+		'ConcurrenTree.model.wrapper',
+		'ConcurrenTree.storage',
+	],
+)
